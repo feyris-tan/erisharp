41d6d46885a83739412a14b06ca985ae62eada09
feyris-tan <welcometotheworld>
2014-10-20 18:52:01 +0200

    Some structs. Let the fun begin.

---
+++ src/erisamatrix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ERIShArp.Matrix
+{
+    public enum DCTDegreeLimit
+    {
+        MIN_DCT_DEGREE = 2,
+        MAX_DCT_DEGREE = 12,
+    }
+
+    public struct ERI_SIN_COS
+    {
+        float rSin;
+        float rCos;
+    }
+}
---
+++ src/xerisa.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ERIShArp.X
+{
+    public struct ERI_FILE_HEADER
+    {
+        uint dwVersion;
+        uint dwContainedFlag;
+        uint dwKeyFrameCount;
+        uint dwFrameCount;
+        uint dwAllFrameTime;
+    }
+
+    public struct ERI_INFO_HEADER
+    {
+	    uint	dwVersion ;
+	    uint	fdwTransformation ;
+	    uint	dwArchitecture ;
+	    uint	fdwFormatType ;
+	    int	nImageWidth ;
+	    int	nImageHeight ;
+	    uint	dwBitsPerPixel ;
+	    uint	dwClippedPixel ;
+	    uint	dwSamplingFlags ;
+        /// <summary>
+        /// Two Integers
+        /// </summary>
+	    int[]	dwQuantumizedBits;
+        /// <summary>
+        /// Two unsigned intergers.
+        /// </summary>
+	    uint[]	dwAllottedBits;
+	    uint	dwBlockingDegree ;
+	    uint	dwLappedBlock ;
+	    uint	dwFrameTransform ;
+	    uint	dwFrameDegree ;
+    }
+
+    public struct MIO_INFO_HEADER
+    {
+        uint dwVersion;
+        uint fdwTransformation;
+        uint dwArchitecture;
+        uint dwChannelCount;
+        uint dwSamplesPerSec;
+        uint dwBlocksetCount;
+        uint dwSubbandDegree;
+        uint dwAllSampleCount;
+        uint dwLappedDegree;
+        uint dwBitsPerSample;
+    }
+
+    public struct MIO_DATA_HEADER
+    {
+        byte bytVersion;
+        byte bytFlags;
+        byte bytReserved1;
+        byte bytReserved2;
+        uint dwSampleCount;
+    } ;
+}
